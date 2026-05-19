1d0e5beec13759db8e0ad91bcd242635b2b6b24e
PIT010 <intern1@ctyhp.vn>
2026-05-19 14:21:19 +0700

    update deploy config

--- app/api/vendors/route.ts
+++ app/api/vendors/route.ts
@@ -1,6 +1,10 @@
 import { makeCrud } from "@/lib/crud";
 import { vendors, insertVendorSchema } from "@shared/schema";
 
-const crud = makeCrud({ table: vendors, insertSchema: insertVendorSchema });
+const crud = makeCrud({
+  table: vendors,
+  insertSchema: insertVendorSchema,
+  requireAdminToWrite: true,
+});
 export const GET = crud.listAll;
 export const POST = crud.createOne;
